79bde272062f6aacf23a996e3f6b3f91c7cdfa90
CarloP91 <carlopennetta91@gmail.com>
2020-08-06 00:05:17 +0200

    mod js

--- js/srcbar_function.js
+++ js/srcbar_function.js
@@ -12,9 +12,10 @@ function srcfnt() {
       txtValue = a.textContent || a.innerText;
       if (txtValue.toUpperCase().indexOf(filter) > -1 && document.getElementById("myInput").value != "") {
         li[i].style.display = "";
-        document.getElementById("hidesrc").style.display = 'block'
+        document.getElementById("hidesrc").style.display = 'block';
       } else {
         li[i].style.display = "none";
+        document.getElementById("hidesrc").innerHTML = Ciao;
       }
     }
   }
