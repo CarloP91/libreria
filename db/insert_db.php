abcf5053d8d7f01f740f6c9790d8d541d5d808a1
CarloP91 <carlopennetta91@gmail.com>
2020-08-05 12:20:19 +0200

    Initial commit

---
+++ db/insert_db.php
@@ -0,0 +1,33 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "book_ccls";
+
+$titolo = $_GET["titolo"];
+$autore = $_GET["autore"];
+$codice = $_GET["codice"];
+$descrizione = $_GET["descrizione"];
+$anno = $_GET["anno"];
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "INSERT INTO book (titolo, autore, codice, anno, descrizione)
+		VALUES ('$titolo', '$autore', '$codice', '$anno', '$descrizione')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "Libro Inserito";
+ header( "refresh:3;url=../index.php" );
+   	 // header("Location: ../index.php");
+
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?>
---
+++ index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>libreria</title>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.1/css/bootstrap.min.css" integrity="sha384-VCmXjywReHh4PwowAiWNagnWcLhlEJLA5buUprzK8rxFgeH0kww/aWY76TfkUoSX" crossorigin="anonymous">
+
+    <link rel="stylesheet" type="text/css" href="style/style.css">
+</head>
+
+<body>
+
+    <img class="img-fluid" src="img/narrative.jpg">
+
+    <select>
+        <option>Ciao</option>
+        <option>Papa</option>
+    </select>
+
+      <div class="container">
+        <div class="row">
+            <div class="col"></div>
+            <div class="col-3">ciao</div>
+            <div class="col"></div>
+        </div>
+    </div>
+
+    <form action="db/insert_db.php" method="GET"> 
+
+        <label class="chiara">TITOLO: </label>
+        <input type="text" name="titolo" value="titolo" required>
+        <br>
+        <label>AUTORE: </label>
+        <input type="text" name="autore" value="autore" required>
+        <br>
+        <label>CODICE: </label>
+        <input type="number" name="codice" value="codice" required>
+        <br>
+        <label>ANNO: </label>
+        <input type="number" name="anno" value="anno" required>
+        <br>
+        <label>DESCRIZIONE: </label>
+        <textarea name="descrizione" value="descrizione"></textarea> 
+        <br>
+        <br>
+        <input type="submit" name="submit" value="salva">
+
+    </form>
+
+
+</body>
+</html>
