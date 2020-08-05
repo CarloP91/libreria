7c8cf642a7b8c1940cd351370a90b978495d921f
CarloP91 <carlopennetta91@gmail.com>
2020-08-05 15:01:02 +0200

    create page

---
+++ libro/create_page.php
@@ -0,0 +1,14 @@
+<?php 
+error_reporting(E_ALL);
+
+$pagename = 'my_page1';
+
+$newFileName = './'.$pagename.".php";
+$newFileContent = '<?php echo "something..."; ?>';
+
+if (file_put_contents($newFileName, $newFileContent) !== false) {
+    echo "File created (" . basename($newFileName) . ")";
+} else {
+    echo "Cannot create file (" . basename($newFileName) . ")";
+}
+?>
