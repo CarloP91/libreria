0e4d6d44f0ddd87120b6d392168787a267502fc6
CarloP91 <carlopennetta91@gmail.com>
2020-08-05 15:14:07 +0200

    create page

--- libro/create_page.php
+++ libro/create_page.php
@@ -1,10 +1,42 @@
 <?php 
 error_reporting(E_ALL);
 
-$pagename = 'my_page1';
+$pagename = 'prova';
 
 $newFileName = './'.$pagename.".php";
-$newFileContent = '<?php echo "something..."; ?>';
+$newFileContent = '<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "book_ccls";
+
+// $src_arrive = $_GET[""];
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+	die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT * FROM book";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+  // output data of each row
+	while($row = $result->fetch_assoc()) {
+		echo  $row["titolo"] ."<br>"
+		.$row["autore"] ."<br>";
+		.$row["codice"] ."<br>";
+		.$row["anno"] ."<br>";
+		.$row["descrizione"] ."<br>";
+
+	}
+} else {
+	echo "0 results";
+}
+$conn->close();
+?>';
 
 if (file_put_contents($newFileName, $newFileContent) !== false) {
     echo "File created (" . basename($newFileName) . ")";
