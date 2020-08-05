1d60e792902d1d3214f1278c968d01aadda43b53
CarloP91 <carlopennetta91@gmail.com>
2020-08-05 22:14:10 +0200

    new searchbar

---
+++ db/select_libro_src.php
@@ -0,0 +1,30 @@
+<?php
+
+require 'log.php';
+
+ $src_arrive = "Harry Potter";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+	die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT * FROM book";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+  // output data of each row
+	while($row = $result->fetch_assoc()) {
+		echo 
+		'<li><a href="#">' .$row["titolo"] .'</a></li> ';
+
+	}
+} else {
+	echo "Nessun Risultato" 
+	."<br>"
+	.'<a href="../libro/inserisci_libro.php">VUOI INSERIRE UN NUOVO LIBRO?</a>';
+}
+$conn->close();
+?>
--- libro/libro.php
+++ libro/libro.php
@@ -5,7 +5,8 @@
 
 <!-- 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.1/css/bootstrap.min.css" integrity="sha384-VCmXjywReHh4PwowAiWNagnWcLhlEJLA5buUprzK8rxFgeH0kww/aWY76TfkUoSX" crossorigin="anonymous"> -->
 
-	<link rel="stylesheet" type="text/css" href="../style/stylegenerico.css">
+	<link rel="stylesheet" type="text/css" href="../style/style_new_srcbar.css">
+	<script src="../js/srcbar_function.js"></script>
 </head>
 <body>
 
@@ -13,7 +14,7 @@
 	<img class="img-fluid" src="../img/narrative.jpg"> -->
 
 
-	<h1> LIBRERIA </h1>
+	<!-- <h1> LIBRERIA </h1>
 	<div class="topnav">
 		<div class="search-container">
 			<form action="libro/libro.php" method="GET">
@@ -21,12 +22,24 @@
 				<button type="submit"><i class="fa fa-search"></i></button>
 			</form>
 		</div>
-	</div>
+	</div> -->
 
-	<div class="copertina">
+	<div><input type="text" id="myInput" onkeyup="myFunction()" placeholder="Search for names..">
+
+<ul id="myUL">
+ <!--  <li><a href="#">Adele</a></li>
+  <li><a href="#">Agnes</a></li>
+
+  <li><a href="#">Billy</a></li>
+  <li><a href="#">Bob</a></li>
+
+  <li><a href="#">Calvin</a></li>
+  <li><a href="#">Christina</a></li>
+  <li><a href="#">Cindy</a></li>-->
+  <?php include '../db/select_libro_src.php'?>
+</ul>  
+</div>
 		
-
-	</div>
 	<?php include '../db/select_libro.php' ?>
 
 </body>
