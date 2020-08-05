d26fbd41f2b51b5d020a21fe6bbd5983b3594321
ludoluvy <69243305+ludoluvy@users.noreply.github.com>
2020-08-05 16:55:41 +0200

    man

--- libro/libro.php
+++ libro/libro.php
@@ -2,8 +2,28 @@
 <html>
 <head>
 	<title></title>
+
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.1/css/bootstrap.min.css" integrity="sha384-VCmXjywReHh4PwowAiWNagnWcLhlEJLA5buUprzK8rxFgeH0kww/aWY76TfkUoSX" crossorigin="anonymous">
+
+	<link rel="stylesheet" type="text/css" href="style/stylegenerico.css">
 </head>
 <body>
- 	<?php include '../db/select_libro.php' ?>
+
+
+	<img class="img-fluid" src="../img/narrative.jpg">
+
+
+	<h1> LIBRERIA </h1>
+	<div class="topnav">
+		<div class="search-container">
+			<form action="libro/libro.php" method="GET">
+				<input type="text" placeholder="libro, autore..." name="search">
+				<button type="submit"><i class="fa fa-search"></i></button>
+			</form>
+		</div>
+	</div>
+	<?php include '../db/select_libro.php' ?>
+
+	<?php  ?>
 </body>
 </html>
