7d55add8a325bd6b3ff6e50082f8f7939c950d66
sndbnc <69243641+sndbnc@users.noreply.github.com>
2020-08-05 15:17:55 +0200

    nociao

--- index.php
+++ index.php
@@ -19,7 +19,7 @@
       <div class="container">
         <div class="row">
             <div class="col"></div>
-            <div class="col-3">ciao</div>
+            <div class="col-3"></div>
             <div class="col"></div>
         </div>
     </div>
