b49b8b1f4f0fce0b91723abb5718f58a9271e8ca
CarloP91 <carlopennetta91@gmail.com>
2020-08-06 11:02:27 +0200

    fix insert bug

--- libro/insert_db.php
+++ libro/insert_db.php
@@ -20,22 +20,10 @@ $sql = "INSERT INTO book (titolo, autore, codice, anno, descrizione)
 VALUES ('$titolo', '$autore', '$codice', '$anno', '$descrizione')";
 
 if ($conn->query($sql) === TRUE) {
-    echo "Libro Inserito";
-    
-    // $string = str_replace(' ', '', $titolo);
-    // $string = strtolower($string);
-    
-    
-    if (file_put_contents($newFileName, $newFileContent) !== false) {
-        echo "File created (" . basename($newFileName) . ")";
-    } else {
-        echo "Cannot create file (" . basename($newFileName) . ")";
-    }
-    header( "refresh:3;url=../index.php" );
-    // header("Location: ../index.php");
-    
+  echo "Libro Inserito";
+   header("Location: arrive.php");
 } else {
-    echo "Error: " . $sql . "<br>" . $conn->error;
+  echo "Error: " . $sql . "<br>" . $conn->error;
 }
 
 $conn->close();
